05eccc2d50583096e41ce63a7804ec8dbcfa9ccf
t-sahsan <t-sahsan@microsoft.com>
2020-07-21 10:10:55 -0700

    Demo Change

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com", 
-            password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test "should be valid" do
@@ -70,6 +69,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
+    password = "thisIsMyPassword"
     assert_not @user.valid?
   end
 end
