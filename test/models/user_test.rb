dbc42e5a8273d9e875cd3943096b259282aa9fc5
t-sahsan <t-sahsan@microsoft.com>
2020-07-21 10:18:46 -0700

    Demo Change

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -69,7 +69,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
-    password = "thisIsMyPassword"
+    User.new(password: "foobar", password_confirmation: "foobar")
     assert_not @user.valid?
   end
 end
